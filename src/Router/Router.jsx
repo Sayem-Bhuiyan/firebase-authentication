c466d3ceece0fd751ac5e41b889787f40cbc4c58
Sayem <sayem465508@gmail.com>
2024-04-06 18:06:56 +0600

    created a private route and showing a spinner for loading data

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,8 @@ import Root from "../MainLayout/Root";
 import Home from "../components/Home/Home";
 import Login from "../components/Login/Login";
 import Register from "../components/Register/Register";
+import Orders from "../components/Orders/Orders";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 
 export const router = createBrowserRouter([
@@ -21,6 +23,10 @@ export const router = createBrowserRouter([
             {
                 path: '/register',
                 element: <Register />
+            },
+            {
+                path: '/orders',
+                element: <PrivateRoute ><Orders /></PrivateRoute>
             }
         ]
     }
--- src/components/providers/AuthProvider.jsx
+++ src/components/providers/AuthProvider.jsx
@@ -7,16 +7,20 @@ export const AuthContext = createContext(null);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     const createUser = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth , email, password)
     }
 
     const loginUser = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
+        setLoading(true)
         return signOut(auth)
     }
 
@@ -24,6 +28,7 @@ const AuthProvider = ({children}) => {
     useEffect(()=> {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
+            setLoading(false)
             console.log('currentUser', currentUser);
         })
 
@@ -34,8 +39,10 @@ const AuthProvider = ({children}) => {
     
 
 
-    const authInfo = {user,
-         createUser,
+    const authInfo = {
+          user,
+          loading,
+          createUser,
           loginUser,
           logOut
         };
