994ec9fbf72a63a16fcfe8b0b8c9bc1714524474
Sayem <sayem465508@gmail.com>
2024-04-06 11:13:56 +0600

    reisterd the user through firebase

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,12 +1,39 @@
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase/firebase.config";
 
 const Register = () => {
 
     const handleRegister = e => {
         e.preventDefault();
+        const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        console.log(name,email, password);
+
+
+        // creating user
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(userCredential => {
+            const user = userCredential.user;
+            console.log(user);
+
+            // updateProfile 
+            updateProfile(user, {
+                displayName: name , photoURL: 'https://i.postimg.cc/D0SdCtvF/1675442698903.jpg'
+            })
+            .then( () => console.log('profile updated'))
+            .catch(error => console.log(error))
+
+            sendEmailVerification(user)
+            .then(() => {
+                alert('varification email sent')
+            })
+            .catch(error => console.log(error))
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 
     return (
@@ -19,12 +46,24 @@ const Register = () => {
           <form onSubmit={handleRegister} className="card-body">
             <div className="form-control">
               <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
                 name="email"
-                placeholder="email"
+                placeholder="Your Email"
                 className="input input-bordered"
                 required
               />
@@ -43,7 +82,7 @@ const Register = () => {
              
             </div>
             <div className="form-control mt-6">
-              <input className="btn btn-primary" type="Register" value="Login" />
+              <input className="btn btn-primary" type="submit" value="Register" />
             </div>
             <p>Already registered? please <Link to="/login" className="underline" >login here</Link></p>
 
